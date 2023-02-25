78d91e1a5e1cbe7676a4ebd80b90c945fdabe039
riteshbenjwal <riteshbenjwal7@gmail.com>
2023-02-25 14:35:18 +0530

    controller setup for tweet

--- src/index.js
+++ src/index.js
@@ -1,9 +1,20 @@
 import express from "express";
 import { connect } from "./config/database.js";
+import apiRoutes from "./routes/index.js";
+import bodyParser from "body-parser";
 const app = express();
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.use("/api", apiRoutes);
+
 app.listen(3000, async () => {
-  console.log("server started");
+  console.log("server started on port 3000");
   await connect();
   console.log("Mongo db connected");
 });
